d6f7f7ebc7705ff010f6541de288337c4388d1eb
W1ck3dgg <presiyan.penev.uk@gmail.com>
2023-12-16 03:20:13 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from './Navbar.module.css';
+import styles from './navbar.module.css';
 
 const Navbar = () => {
   return (
